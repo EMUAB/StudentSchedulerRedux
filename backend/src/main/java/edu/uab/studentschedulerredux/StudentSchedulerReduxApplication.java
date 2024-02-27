cb86425eee1357ddd8934f3ea6f7c4ecebb1ccc3
genDevo <113210196+Monforton@users.noreply.github.com>
2024-02-27 16:45:24 -0600

    Init repo

---
+++ backend/src/main/java/edu/uab/studentschedulerredux/StudentSchedulerReduxApplication.java
@@ -0,0 +1,21 @@
+package edu.uab.studentschedulerredux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudentSchedulerReduxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSchedulerReduxApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String apiRoot() {
+        return "test stuff :)";
+    }
+
+}
---
+++ backend/src/test/java/edu/uab/studentschedulerredux/StudentSchedulerReduxApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uab.studentschedulerredux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSchedulerReduxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
