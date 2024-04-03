95d7db45aee4a78b0cc74ca7f92feb1c12a6a00e
Vaishak Menon <vaishakkmenon@gmail.com>
2024-04-03 12:20:58 -0500

    removed server-starts

--- server-start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd Server
-set MONGO_DB_PASSWORD=OaxM9eobBOToHaOy
-mvn package
-java -jar target\StudentSchedulerRedux-0.0.1-SNAPSHOT.jar
--- server-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd Server
-export MONGO_DB_PASSWORD='OaxM9eobBOToHaOy'
-mvn package
-java -jar target/StudentSchedulerRedux-0.0.1-SNAPSHOT.jar
