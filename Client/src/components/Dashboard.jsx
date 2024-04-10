ebb4e2a714854dc91feb977e111955fb53b4bf1d
Monforton <113210196+Monforton@users.noreply.github.com>
2024-04-09 20:05:32 -0500

    Refactor/restructure possible schedule list

--- Client/src/components/Dashboard.jsx
+++ Client/src/components/Dashboard.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import React from "react";
+import { React, useState } from 'react'
 import moment from "moment";
 import RefreshIcon from '@mui/icons-material/Refresh';
 import "./Dashboard.css";
-import Modal from 'react-modal';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import DashNavbar from "./Navbar";
-import { Button, Card, Form } from "react-bootstrap";
+import { Button, Card, Form, Modal } from "react-bootstrap";
 import { possibleSchedData, possibleTerms } from "./possible-sched-data";
-import Column from "./Column";
 import { logout } from '../AuthService';
+import Calendar from './Calendar';
+import ScheduleList from './ScheduleList';
 
 function Dashboard() {
 
-  const [changeTermModalOpen, setChangeTermModalOpen] = useState(false);
+  const [isChangeTermModalOpen, setChangeTermModalOpen] = useState(false);
+  const [isPreviewScheduleModalOpen, setPreviewScheduleModalOpen] = useState(false);
   const [advisorName, setAdvisorName] = useState("John Doe");
   const [events, setEvents] = useState([
     {
@@ -24,27 +24,25 @@ function Dashboard() {
       title: "Some title"
     }
   ]);
-  const [possibleSchedules, setPossibleSchedules] = useState(possibleSchedData.schedules);
   const [terms, setTerms] = useState(possibleTerms.terms);
-  const [selectedSchedule, setSelectedSchedule] = useState(null);
+  const [selectedSchedule, setSelectedSchedule] = useState(possibleSchedData.schedules[0]);
   const [selectedTerm, setSelectedTerm] = useState(terms[0]);
   const [tempTermID, setTempTermID] = useState(terms[0]);
 
 
-  const closeModal = (chosenTerm) => {
-    if (chosenTerm) {
+  const closeChangeTermModal = (isTermChanged) => {
+    if (isTermChanged) {
       setSelectedTerm(terms.find(term => term.id === tempTermID));
     }
     setChangeTermModalOpen(false);
   };
 
-  const openModal = () => {
-    setChangeTermModalOpen(true);
+  const closePreviewScheduleModal = (isScheduleSelected) => {
+    if (isScheduleSelected) {
+      setSelectedSchedule();
+    }
+    setPreviewScheduleModalOpen(false);
   };
-
-  const afterOpenModal = () => {
-    // references are now sync'd and can be accessed.
-  }
 
   const handleLogout = () => {
     logout();
@@ -60,34 +58,26 @@ function Dashboard() {
             <Card.Body>
               <Card.Title style={{ fontSize: '1.8rem', fontWeight: 'bold' }}>{selectedTerm.name}</Card.Title>
               <Card.Text>
-                <Button variant="light" size="sm" onClick={openModal}>Change term</Button>
-                <Modal
-                  isOpen={changeTermModalOpen}
-                  onAfterOpen={afterOpenModal}
-                  onRequestClose={closeModal}
-                  style={{
-                    content: {
-                      top: '50%',
-                      left: '50%',
-                      right: 'auto',
-                      bottom: 'auto',
-                      transform: 'translate(-50%, -50%)',
-                    },
-                  }}
-                  contentLabel="Change Term Modal"
-                >
-                  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '30rem' }}>
-                    <h2>Change Term</h2>
-                    <Form.Select aria-label="Term selection form" onChange={(e) => setTempTermID(e.target.value)}>
-                      {terms.map((term, index) => (
-                        <option key={index} value={term.id} selected={term.id === selectedTerm.id}>{term.name}</option>
-                      ))}
-                    </Form.Select>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', paddingTop: '1rem' }}>
-                      <Button variant="primary" onClick={() => closeModal(true)} style={{ width: '8rem' }}>Select</Button>
-                      <Button variant="secondary" onClick={() => closeModal(false)} style={{ width: '8rem' }}>Cancel</Button>
-                    </div>
-                  </div>
+                <Button variant="light" size="sm" onClick={() => setChangeTermModalOpen(true)}>Change term</Button>
+                <Modal size="md" show={isChangeTermModalOpen} onHide={() => setChangeTermModalOpen(false)} centered>
+                  <Modal.Dialog style={{
+                    display: 'flex', width: '100%', margin: '0px', fontFamily: 'Inter, sans-serif',
+                  }}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Change Term</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <Form.Select aria-label="Term selection form" onChange={(e) => setTempTermID(e.target.value)}>
+                        {terms.map((term, index) => (
+                          <option key={index} value={term.id} selected={term.id === selectedTerm.id}>{term.name}</option>
+                        ))}
+                      </Form.Select>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={() => closeChangeTermModal(false)}>Cancel</Button>
+                      <Button variant="primary" onClick={() => closeChangeTermModal(true)}>Select</Button>
+                    </Modal.Footer>
+                  </Modal.Dialog>
                 </Modal>
               </Card.Text>
             </Card.Body>
@@ -133,14 +123,35 @@ function Dashboard() {
           <Card style={{ width: '100%', marginRight: '1rem', marginLeft: '0.5rem', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)', backgroundColor: "#1b604a", color: "#fff" }}>
             <Card.Body>
               <Card.Title style={{ fontSize: '1.8rem', fontWeight: 'bold', display: 'flex' }}>
-                <p>Possible Schedules</p>
+                <p style={{ marginBottom: '4px' }}>Possible Schedules</p>
                 <Button variant="light" size="sm" style={{ marginLeft: 'auto', alignSelf: 'flex-start' }} href="#">
                   <RefreshIcon /> Refresh
                 </Button>
               </Card.Title>
-              <Card.Text>
-                <Column schedules={possibleSchedules} />
-              </Card.Text>
+              <ScheduleList />
+              <Modal size="md" show={isPreviewScheduleModalOpen} onHide={() => setChangeTermModalOpen(false)} centered>
+                <Modal.Dialog style={{
+                  display: 'flex', width: '100%', margin: '0px', fontFamily: 'Inter, sans-serif',
+                }}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Preview Schedule</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '50rem', height: '50rem' }}>
+                      <h2>Schedule Preview</h2>
+                      <Calendar />
+                      <div style={{ display: 'flex', justifyContent: 'space-between', paddingTop: '1rem' }}>
+                        <Button variant="primary" onClick={() => setSelectedSchedule(null)} style={{ width: '10rem', height: '2.5rem', marginTop: '1rem' }}>Choose Schedule</Button>
+                        <Button variant="secondary" onClick={() => setPreviewScheduleModalOpen(false)} style={{ width: '6rem', marginTop: '1rem' }}>Cancel</Button>
+                      </div>
+                    </div>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={() => closeChangeTermModal(false)}>Cancel</Button>
+                    <Button variant="primary" onClick={() => closeChangeTermModal(true)}>Select</Button>
+                  </Modal.Footer>
+                </Modal.Dialog>
+              </Modal>
             </Card.Body>
           </Card>
         </div>
